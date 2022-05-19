7945935e5ee69454801a67e44d3098773df397b9
nudago <nayohan13@gmail.com>
2022-05-19 12:23:01 +0000

    [ADD] download dataset and model

---
+++ run.sh
@@ -0,0 +1,81 @@
+## 1. Data Augment Process
+# BERT_BASE_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/bert-base-uncased'
+# GLOVE_EMB='/home/uj-user/Pretrained-Language-Model/TinyBERT/glove.6B.300d.txt'
+# GLUE_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/glue_data'
+# TASK_NAME='SST-2'
+# python data_augmentation.py --pretrained_bert_model ${BERT_BASE_DIR} --glove_embs ${GLOVE_EMB} --glue_dir ${GLUE_DIR} --task_name ${TASK_NAME}
+
+
+## 2. Setting Task Path & Model
+# ${FT_BERT_BASE_DIR}$ contains the fine-tuned BERT-base model.
+# FT_BERT_BASE_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/bert-base-uncased'
+# GENERAL_TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert'
+# TASK_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/glue_data/RTE'
+# TASK_NAME='rte'
+# TMP_TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert-RTE-tmp'
+# TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert-RTE'
+# OUTPUT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/output_RTE'
+
+FT_BERT_BASE_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/bert-base-uncased-SST-2'
+GENERAL_TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert'
+TASK_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/glue_data/SST-2'
+TASK_NAME='sst-2'
+TMP_TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert-SST-2-tmp-aug'
+TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert-SST-2-aug'
+OUTPUT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/output_SST-2-aug'
+
+# FT_BERT_BASE_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/bert-base-uncased'
+# GENERAL_TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert'
+# TASK_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/glue_data/MRPC'
+# TASK_NAME='mrpc'
+# TMP_TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert-MRPC-tmp'
+# TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert-MRPC'
+# OUTPUT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/output_MRPC'
+
+# FT_BERT_BASE_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/bert-base-uncased'
+# GENERAL_TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert'
+# TASK_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/glue_data/MNLI_bak'
+# TASK_NAME='mnli'
+# TMP_TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert-MNLI-tmp'
+# TINYBERT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/tiny-bert-MNLI'
+# # OUTPUT_DIR='/home/uj-user/Pretrained-Language-Model/TinyBERT/output-MNLI'
+
+
+## 3. Finetune TinyBERT + Layer Distillation
+# python task_distill.py --teacher_model ${FT_BERT_BASE_DIR} \
+#                        --student_model ${GENERAL_TINYBERT_DIR} \
+#                        --data_dir ${TASK_DIR} \
+#                        --task_name ${TASK_NAME} \
+#                        --output_dir ${TMP_TINYBERT_DIR} \
+#                        --max_seq_length 128 \
+#                        --train_batch_size 256 \
+#                        --num_train_epochs 10 \
+#                        --aug_train \
+#                        --do_lower_case
+
+
+## 4. Finetune TinyBERT + Predction Layer
+python task_distill.py --pred_distill \
+                       --teacher_model ${FT_BERT_BASE_DIR} \
+                       --student_model ${TMP_TINYBERT_DIR} \
+                       --data_dir ${TASK_DIR} \
+                       --task_name ${TASK_NAME} \
+                       --output_dir ${TINYBERT_DIR} \
+                       --do_lower_case \
+                       --learning_rate 3e-5  \
+                       --num_train_epochs  10  \
+                       --eval_step 100 \
+                       --max_seq_length 128 \
+                        --aug_train \
+                       --train_batch_size 256
+
+
+## 5. Evaluate Finetune TinyBERT
+# python task_distill.py --do_eval \
+#                        --student_model ${TINYBERT_DIR} \
+#                        --data_dir ${TASK_DIR} \
+#                        --task_name ${TASK_NAME} \
+#                        --output_dir ${OUTPUT_DIR} \
+#                        --do_lower_case \
+#                        --eval_batch_size 32 \
+#                        --max_seq_length 128
